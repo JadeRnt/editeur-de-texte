3b2a1c4cd4043cb938c36bc7f73a5586969b5a8d
Charlotte <charlotte.lewis@etu.umontpellier.fr>
2023-11-06 09:43:03 +0100

    Ajouter le fonctio remplacer

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,27 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    public void executer() {
+        if(parameters.length < 2) {
+            System.err.println("Format attendu : ajouter;texte");
+            return;
+        }
+        int deb = Integer.parseInt(parameters[1]);
+        int fin = Integer.parseInt(parameters[2]);
+        if (parameters.length == 4) {
+            String texte = parameters[3];
+            this.document.remplacer(deb, fin, texte);
+        }
+        else {
+            this.document.remplacer(deb, fin, "");
+        }
+        super.executer();
+    }
+}
