b526995303f30388bca7765c9475d224eeb92c87
Charlotte <charlotte.lewis@etu.umontpellier.fr>
2023-11-06 10:33:48 +0100

    debut de executer

---
+++ src/main/java/fr/iut/editeur/commande/CommandeClear.java
@@ -0,0 +1,13 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeClear extends CommandeDocument{
+    public CommandeClear(Document doc, String[] parametres) {
+        super(doc, parametres);
+    }
+
+    public void executer() {
+
+    }
+}
