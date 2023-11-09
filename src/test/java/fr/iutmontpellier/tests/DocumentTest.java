4571fcdeb5e98f601009c5f0fd439560a1de48ec
renautj <jade.renaut@etu.umontpellier.fr>
2023-11-09 11:43:29 +0100

    ex 5

---
+++ src/test/java/fr/iutmontpellier/tests/DocumentTest.java
@@ -0,0 +1,17 @@
+package fr.iutmontpellier.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DocumentTest {
+    @Test
+    public void test1() {
+        assertEquals(true,true);
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(true,false);
+    }
+}
